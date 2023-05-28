4b4d64df70145af16972e5c004312c701afed61a
alexandrerosa-dev <asr.alexandrerosa@gmail.com>
2023-05-28 18:13:39 -0300

    comentários

--- aws_default_route_table.tf
+++ aws_default_route_table.tf
@@ -1,5 +1,6 @@
 resource "aws_default_route_table" "route_table_default" {
     default_route_table_id = aws_vpc.my_vpc.default_route_table_id
+#configuração da tabela de roteamento
 
     route {
         cidr_block = "0.0.0.0/0"
@@ -10,6 +11,7 @@ resource "aws_default_route_table" "route_table_default" {
         local.common_tags,
         {
             Name = format("%s_route_table", var enviroment)
+            #%s pega a primeira variável após a virgula e substitui
         },
     )
 }
