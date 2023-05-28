3fb280de9f64d74e89e0587e0b60f78c01cdf5fd
alexandrerosa-dev <asr.alexandrerosa@gmail.com>
2023-05-28 17:54:13 -0300

    configuração da subnet

--- locals.tf
+++ locals.tf
@@ -7,13 +7,13 @@ locals {
     network_prefix = lookup(local.cidr_block_prefix, var.enviroment)
 
     subnet_public = {
-        1 ={ cidr_block = "${local.network_prefix}.1.0/24", availability_zone }
-        2 ={ cidr_block = "${local.network_prefix}.2.0/24", availability_zone }
+        1 ={ cidr_block = "${local.network_prefix}.1.0/24", availability_zone = "${var.region}a"}
+        2 ={ cidr_block = "${local.network_prefix}.2.0/24", availability_zone = "${var.region}b" }
     }
 
     subnet_public = {
-        1 ={ cidr_block = "${local.network_prefix}.3.0/24", availability_zone }
-        2 ={ cidr_block = "${local.network_prefix}.4.0/24", availability_zone }
+        1 ={ cidr_block = "${local.network_prefix}.3.0/24", availability_zone = "${var.region}a" }
+        2 ={ cidr_block = "${local.network_prefix}.4.0/24", availability_zone = "${var.region}b" }
     }
 
     common_tags = {
